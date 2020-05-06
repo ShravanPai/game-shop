02c75bb971bb6b35814a052dd81207ace32a67af
Shravan Pai <glxpai@gmail.com>
2020-05-05 23:25:14 -0700

    CODE: Adding some UI components

--- src/index.js
+++ src/index.js
@@ -1,67 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {current_game: ''};
+
+    this.handleGameSelect = this.handleGameSelect.bind(this);
   }
-}
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+  handleGameSelect(event) {
+    this.setState({current_game: event.target.value})
   }
 
   render() {
-    const status = 'Next player: X';
-
     return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+      <React.Fragment>
+      <h1 align="center"> Welcome to Raja's Game Room </h1>
+      <Button variant="primary" size="lg" block onClick={this.handleGameSelect} value="donkey"> Donkey </Button>
+      <Button variant="secondary" size="lg" block onClick={this.handleGameSelect} value="uno"> Uno </Button>
+      </React.Fragment>
+      
     );
   }
 }
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
 
 ReactDOM.render(
-  <Game />,
+  <NameForm />,
   document.getElementById('root')
 );
