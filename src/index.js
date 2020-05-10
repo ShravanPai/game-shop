d9c4d4111ea4c37c7d3d563afd644ad05010a5bf
Shravan Pai <glxpai@gmail.com>
2020-05-10 12:01:38 -0700

    CODE: Adding more components and REST API to fetch game rooms

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import Donkey from './donkey';
 
 class GameRoom extends React.Component {
@@ -11,6 +12,7 @@ class GameRoom extends React.Component {
     this.state = { current_game: '', game_selected: false };
 
     this.handleGameSelect = this.handleGameSelect.bind(this);
+    this.leaveGame = this.leaveGame.bind(this);
   }
 
   handleGameSelect(event) {
@@ -19,46 +21,50 @@ class GameRoom extends React.Component {
       this.setState({ game_selected: true });
     else
       this.setState({ game_selected: false });
+
+
+    if (event.target.value == 'Donkey')
+      this.setState({current_game: <Donkey />})
+  }
+
+  leaveGame() {
+      if (window.confirm('Leave Game?')) {
+        this.setState({current_game: null})
+        this.setState({game_selected: false})
+      }
   }
 
   render() {
-
-    let game;
-    if (this.state.current_game == 'Donkey')
-      game = <Donkey />;
-
     return (
       <React.Fragment>
         <h1 align="center"> Welcome to Raja's Game Room </h1>
-        {!this.state.game_selected ? 
-        <Form>
-          <Form.Group controlId="gameShopForm.GameSelect1">
-            <Form.Label>What do you want to play?</Form.Label>
-            <Form.Control as="select" onChange={this.handleGameSelect}>
-              <option>--None--</option>
-              <option>Donkey</option>
-              <option>Uno</option>
-            </Form.Control>
-          </Form.Group>
-        </Form>
-        : null }
-
+        {!this.state.game_selected ? <HomeScreenForm onGameSelect={this.handleGameSelect} /> : null }
         {this.state.game_selected ?
-          <Form>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Enter passcode</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Button variant="primary" type="submit">Authenticate</Button>
-          </Form>
+          <div align="right">
+          <Button variant="outline-danger" size="sm" onClick={this.leaveGame}>Leave Game</Button>
+          </div>
           : null
         }
 
-       {game} 
-
-        
-
+       {this.state.current_game}
       </React.Fragment>
+    );
+  }
+}
+
+class HomeScreenForm extends React.Component {
+  render() {
+    return (
+      <Form>
+      <Form.Group controlId="gameShopForm.GameSelect1">
+        <Form.Label>What do you want to play?</Form.Label>
+        <Form.Control as="select" onChange={this.props.onGameSelect}>
+          <option>--None--</option>
+          <option>Donkey</option>
+          <option>Uno</option>
+        </Form.Control>
+      </Form.Group>
+    </Form>
     );
   }
 }
