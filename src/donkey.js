782dc3977ccffdaa7d1b78c326fa3d5dc9c09776
Shravan Pai <glxpai@gmail.com>
2020-05-18 23:49:53 -0700

    CODE: Refactoring the UI

--- src/donkey.js
+++ src/donkey.js
@@ -1,10 +1,4 @@
 import React from 'react';
-import Image from 'react-bootstrap/Image';
-import Button from 'react-bootstrap/Button';
-import Card from './card'
-import axios from 'axios'
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
-import Tooltip from 'react-bootstrap/Tooltip'
 
 class Donkey extends React.Component {
     constructor(props) {
@@ -13,71 +7,21 @@ class Donkey extends React.Component {
         this.state = {
             my_cards: null,
             card_deck: null,
-            game_started: false,
-            game_rooms: [],
-            current_game_room: null
         };
 
     }
 
-    componentDidMount() {
-        this.fetchGameRooms();
-    }
-
-    fetchGameRooms() {
-        // Query game room backend api to fetch the list of game rooms
-        axios.get("http://127.0.0.1:9090/donkey/game-rooms")
-            .then((response) => {
-                this.setState({ game_rooms: response.data });
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
-
     render() {
         return (
             <React.Fragment>
-                <h3 align="center"> Welcome to Donkey </h3>
-                {!this.state.game_started ? <GameRooms hosts={this.state.game_rooms} gameStarted="false" /> : null}
+                <h3 align="center"> Welcome to Donkey </h3> <br />
+                <h4 align="right"> Game host: {this.props.hostName} </h4>
 
             </React.Fragment>
         );
     }
 }
 
-class GameRooms extends React.Component {
-    constructor(props) {
-        super(props);
-        this.renderTooltip = this.renderTooltip.bind(this);
-    }
 
-    renderTooltip(host) {
-
-        return (
-            <Tooltip id="button-tooltip" placement="auto-end">
-              Room created by {host}
-            </Tooltip>
-          );
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <h4>Join a game room: </h4> <br />
-                {this.props.gameStarted == 'false' ?
-                    this.props.hosts.map((value, index) => {
-                        return <OverlayTrigger
-                            placement="bottom"
-                            delay={{ show: 250, hide: 400 }}
-                            overlay={() => this.renderTooltip(value.host)}>
-                            <Button variant="outline-primary"> {value.name} </Button>
-                        </OverlayTrigger>
-                    })
-                    : null}
-            </React.Fragment>
-        );
-    }
-}
 
 export default Donkey
--- src/index.js
+++ src/index.js
@@ -3,16 +3,43 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
 import Donkey from './donkey';
+import axios from 'axios'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
 
-class GameRoom extends React.Component {
+/*
+class GameRooms extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { current_game: '', game_selected: false };
+    this.state = {
+      host_name: null,
+      game_rooms: [],
+      game_selectd: false,
+      current_game: null,
+      current_game_room: null,
+      host_is_me: false
+    };
 
+    this.renderTooltip = this.renderTooltip.bind(this);
+    this.createRoom = this.createRoom.bind(this);
+    this.setRoomName = this.setRoomName.bind(this);
+    this.setHostName = this.setHostName.bind(this);
+    this.fetchGameRooms = this.fetchGameRooms.bind(this);
     this.handleGameSelect = this.handleGameSelect.bind(this);
     this.leaveGame = this.leaveGame.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchGameRooms();
+  }
+
+  renderTooltip(host) {
+    return (
+      <Tooltip id="button-tooltip" placement="auto-end">
+        Room created by {host}
+      </Tooltip>
+    );
   }
 
   handleGameSelect(event) {
@@ -24,52 +51,135 @@ class GameRoom extends React.Component {
 
 
     if (event.target.value == 'Donkey')
-      this.setState({current_game: <Donkey />})
+      this.setState({ current_game: <Donkey /> })
   }
 
   leaveGame() {
-      if (window.confirm('Leave Game?')) {
-        this.setState({current_game: null})
-        this.setState({game_selected: false})
-      }
+    if (window.confirm('Leave Game?')) {
+      this.setState({ current_game: null })
+      this.setState({ game_selected: false })
+    }
+  }
+
+  createRoom(event) {
+    axios.post('http://localhost:9090/donkey/game-room', { name: this.state.room_name, host: this.state.host_name }, { headers: { "Access-Control-Allow-Origin": "*", "Content-Type": 'application/json' } }).
+      then(() => {
+        this.setState({ current_game_room: this.state.room_name });
+        this.setState({ host_is_me: true });
+      }).
+      catch(function (error) {
+        console.log('Error here: ' + error);
+      });
+  }
+
+  fetchGameRooms() {
+    // Query game room backend api to fetch the list of game rooms
+    axios.get("http://localhost:9090/donkey/game-rooms")
+      .then((response) => {
+        console.log(response);
+        const roomMap = response.data;
+        let roomDetailList = [];
+        for (var roomName in roomMap)
+          roomDetailList.push({ name: roomName, host: roomMap[roomName].host })
+
+        this.setState({ game_rooms: roomDetailList });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  setRoomName(event) {
+    this.setState({ room_name: event.target.value })
+  }
+
+  setHostName(event) {
+    this.setState({ host_name: event.target.value });
   }
 
   render() {
     return (
       <React.Fragment>
         <h1 align="center"> Welcome to Raja's Game Room </h1>
-        {!this.state.game_selected ? <HomeScreenForm onGameSelect={this.handleGameSelect} /> : null }
-        {this.state.game_selected ?
+        {this.state.current_game_room == null ? <HomeScreenForm onGameSelect={this.handleGameSelect} /> : null}
+        {this.state.current_game_room != null ?
           <div align="right">
-          <Button variant="outline-danger" size="sm" onClick={this.leaveGame}>Leave Game</Button>
+            <Button variant="outline-danger" size="sm" onClick={this.leaveGame}>Leave Game</Button>
           </div>
           : null
         }
 
-       {this.state.current_game}
+        <h4>Join a game room: </h4> <br />
+      
+        {this.state.game_selected ? <React.Fragment>
+          <Form>
+            <Form.Group controlId="formBasicGameRoom">
+              <Form.Label>Create Room</Form.Label>
+              <Form.Control type="text" placeholder="Enter room name" onChange={this.setRoomName} />
+              <Form.Control type="text" placeholder="Your name" onChange={this.setHostName} />
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={this.createRoom}>
+              Create Room
+                  </Button>
+          </Form>
+
+          </React.Fragment>
+          : null}
+      </React.Fragment>
+    );
+  }
+}
+*/
+
+class GameShop extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {game_selected: false,
+                  current_game: null};
+
+    this.onGameSelect = this.onGameSelect.bind(this);
+    this.leaveGame = this.leaveGame.bind(this);
+  }
+
+  onGameSelect(event) {
+    this.setState({game_selected: true, current_game: event.target.value});
+  }
+
+  leaveGame() {
+    if (window.confirm('Leave Game?'))
+      this.setState({game_selected: false, current_game: null});
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+      <h1 align="center"> Welcome to Raja's Game Room </h1>
+
+      {/* If game is not selected, show the games dropdown */}
+      {!this.state.game_selected ? 
+        <Form>
+        <Form.Group controlId="gameShopForm.GameSelect1">
+          <Form.Label>What do you want to play?</Form.Label>
+          <Form.Control as="select" onChange={this.onGameSelect}>
+            <option>--None--</option>
+            <option>Donkey</option>
+            <option>Uno</option>
+          </Form.Control>
+        </Form.Group>
+        </Form>
+        : 
+          <div align="right">
+            <Button variant="outline-danger" size="sm" onClick={this.leaveGame}>Leave Game</Button>
+          </div>
+      }
+
+
       </React.Fragment>
     );
   }
 }
 
-class HomeScreenForm extends React.Component {
-  render() {
-    return (
-      <Form>
-      <Form.Group controlId="gameShopForm.GameSelect1">
-        <Form.Label>What do you want to play?</Form.Label>
-        <Form.Control as="select" onChange={this.props.onGameSelect}>
-          <option>--None--</option>
-          <option>Donkey</option>
-          <option>Uno</option>
-        </Form.Control>
-      </Form.Group>
-    </Form>
-    );
-  }
-}
-
 ReactDOM.render(
-  <GameRoom />,
+  <GameShop />,
   document.getElementById('root')
 );
